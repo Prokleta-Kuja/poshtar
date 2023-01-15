12c9c9e4fefc6aad70bfb4382964600eceab030c
Prokleta-Kuja <Prokleta.Kuja@gmail.com>
2023-01-15 06:55:38 +0000

    Routing

--- src/client-app/src/main.ts
+++ src/client-app/src/main.ts
@@ -1,5 +1,36 @@
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 import "./styles.scss";
 import App from "./App.vue";
+import Index from "./pages/Index.vue";
+import IndexId from "./pages/IndexId.vue";
+import "~bootstrap";
 
-createApp(App).mount("#app");
+const router = createRouter({
+  linkActiveClass: "active",
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "route.root",
+      component: Index,
+    },
+    {
+      path: "/domains",
+      name: "route.domains",
+      component: Index,
+    },
+    {
+      path: "/domains/:id",
+      name: "route.domain",
+      component: IndexId,
+    },
+    {
+      path: "/users",
+      name: "route.users",
+      component: Index,
+    },
+  ],
+});
+
+createApp(App).use(router).mount("#app");
