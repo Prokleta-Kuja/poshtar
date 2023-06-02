703af5d304ea0ceaae64b68d82b23419ada1bd9c
Prokleta-Kuja <Prokleta.Kuja@gmail.com>
2023-06-02 07:23:10 +0000

    Condensed transactions column

--- src/client-app/src/pages/Index.vue
+++ src/client-app/src/pages/Index.vue
@@ -88,7 +88,7 @@ refresh();
                 <Header :params="data.params" :on-sort="refresh" column="countryCode" display="Code" />
                 <Header :params="data.params" :on-sort="refresh" column="asn" display="ASN" />
                 <Header :params="data.params" :on-sort="refresh" column="client" />
-                <Header :params="data.params" :on-sort="refresh" column="from" />
+                <!-- <Header :params="data.params" :on-sort="refresh" column="from" /> -->
                 <th></th>
             </thead>
             <tbody>
@@ -149,15 +149,7 @@ refresh();
                         </span>
                     </td>
                     <td class="user-select-all text-nowrap" :title="item.ipAddress ?? ''">{{ item.asn }}</td>
-                    <td class="user-select-all text-nowrap">{{ item.client }}</td>
-                    <td>
-                        <svg v-if="item.username" :title="item.username" xmlns="http://www.w3.org/2000/svg" width="16"
-                            height="16" fill="currentColor" class="bi bi-person-badge-fill me-2" viewBox="0 0 16 16">
-                            <path
-                                d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z" />
-                        </svg>
-                        <span class="user-select-all text-nowrap">{{ item.from }}</span>
-                    </td>
+                    <td class="user-select-all text-nowrap" :title="item.from ?? ''">{{ item.client }}</td>
                     <td class="text-end p-1">
                         <div class="btn-group" role="group">
                             <button class="btn btn-sm btn-primary" @click="showLogs(item.id)" title="Logs">
