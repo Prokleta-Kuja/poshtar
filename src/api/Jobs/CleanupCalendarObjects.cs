188b1687844e272d3ffa3e7e67fa9b0181d83050
Prokleta-Kuja <Prokleta.Kuja@gmail.com>
2023-11-16 04:32:38 +0000

    Cleanup expired calendar objects

--- src/api/Jobs/CleanupCalendarObjects.cs
+++ src/api/Jobs/CleanupCalendarObjects.cs
@@ -21,12 +21,22 @@ public class CleanupCalendarObjects
         if (!before.HasValue)
             before = DateTime.UtcNow.Date.AddDays(-14);
 
-        _logger.LogInformation("Cleaning up calendar object(s) before {Date}", before);
-        var fileNames = await _db.CalendarObjects.Where(co => co.Deleted.HasValue && co.Deleted < before).Select(co => co.FileName).ToListAsync();
+        // Mark calendar objects exipred 14 days ago as deleted
+        var now = DateTime.UtcNow;
+        var markAffected = await _db.CalendarObjects
+            .Where(co => !co.Deleted.HasValue && co.LastOccurence.HasValue && co.LastOccurence.Value < before)
+            .ExecuteUpdateAsync(setters => setters.SetProperty(co => co.Deleted, now).SetProperty(co => co.Modified, now), token);
+        if (markAffected > 0)
+            _logger.LogInformation("Marked {Count} expired calendar object(s) as deleted", markAffected);
+
+        // Permanently delete calendar objects marked for deletion 14 days ago
+        System.Linq.Expressions.Expression<Func<CalendarObject, bool>> calendarObjDeletedBefore = co => co.Deleted.HasValue && co.Deleted < before;
+        var fileNames = await _db.CalendarObjects.Where(calendarObjDeletedBefore).Select(co => co.FileName).ToListAsync(token);
         foreach (var fileName in fileNames)
             File.Delete(C.Paths.CalendarObjectsDataFor(fileName));
 
-        var affected = await _db.CalendarObjects.Where(co => co.Deleted.HasValue && co.Deleted < before).ExecuteDeleteAsync(token);
-        _logger.LogInformation("Cleaned up {Count} calendar object(s)", affected);
+        var deleteAffected = await _db.CalendarObjects.Where(calendarObjDeletedBefore).ExecuteDeleteAsync();
+        if (deleteAffected > 0)
+            _logger.LogInformation("Permanently deleted {Count} calendar object(s)", deleteAffected);
     }
 }
